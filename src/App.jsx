d191524c85f184cecf66925cff9d4d5a09d3b146
kennankole <kennankole@gmail.com>
2023-11-24 09:29:14 +0300

    Creating CustomerLogin component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import AgentLoginForm from './components/agent/AgentLoginForm';
 import ErrorPage from './ErroPage';
+import CustomerLoginForm from './components/customers/CustomerLogin';
 
 const router = createBrowserRouter([
   {
-    path: "agent/",
+    path: "/",
     element: <AgentLoginForm />,
     errorElement: <ErrorPage />,
-  }
+  },
+  {
+    path: "customer/",
+    element: <CustomerLoginForm />,
+  },
 ]);
 export default router;
