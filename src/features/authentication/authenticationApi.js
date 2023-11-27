4a3cfe1b7c3d8a528da0482d4332df2a9472685c
kennankole <kennankole@gmail.com>
2023-11-27 12:05:57 +0300

    Changin the endpoing from development to production

--- src/features/authentication/authenticationApi.js
+++ src/features/authentication/authenticationApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  // baseURL: 'http://localhost:4000',
+  baseURL: 'https://branch-messaging-app-backend.onrender.com/',
   'Content-Type': 'application/json',
 });
 
